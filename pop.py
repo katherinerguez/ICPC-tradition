25672243caad1bacba671186fb45f65b7cd6b094
Jennyfer2004 <jennifer.ss2004@gmail.com>
2024-05-03 11:41:44 -0400

    repair pop

--- pop.py
+++ pop.py
@@ -3,7 +3,7 @@ import json
 from bs4 import BeautifulSoup
 import pandas as pd
 
-def sracp(url):
+def sracp(url,c):
 # Realizar la solicitud GET a la URL
     response = requests.get(url)
     html = response.text
@@ -23,6 +23,8 @@ def sracp(url):
 # Crear un DataFrame
     df = pd.DataFrame(data, columns=['Rank', 'Country', 'Team', 'Score', 'Penalty', 'Prize'])
     df= df[['Rank','Country', 'Team']]
+    df= df.head(50)
+
     df[['University', 'Participants']] = df['Team'].str.extract(r'(.+)\)\:(.+)')
 # Eliminar los caracteres ':' y ',' de la lista de participantes
 
@@ -35,7 +37,7 @@ def sracp(url):
             return value
         else:
             return [value]
-
+    df["Anno"]=c
     df['Participants'] = df['Participants'].apply(convert_to_list)
     df.set_index('University', inplace=True)
     json2019 = df.T.to_json()
@@ -46,7 +48,7 @@ for item in range(0,15):
     c=2023-item
     url=f'https://cphof.org/standings/icpc/{c}'
     print(url)
-    data=sracp(url)
+    data=sracp(url,c)
     if data is not None:
         json_data[c] = data
 nombre_archivo = "datos.json"
