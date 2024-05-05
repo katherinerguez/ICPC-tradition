94531aa8398db165ed069f0bf63d49a83310aa50
Reinicg <lopezrabinoli@gmail.com>
2024-05-04 20:13:47 -0400

    Cambios en el scraping y la estructura del json resultante

--- pop.py
+++ pop.py
@@ -22,8 +22,8 @@ def sracp(url,c):
 
 # Crear un DataFrame
     df = pd.DataFrame(data, columns=['Rank', 'Country', 'Team', 'Score', 'Penalty', 'Prize'])
-    df= df[['Rank','Country', 'Team']]
-    df= df.head(50)
+    df= df[['Rank','Country', 'Team','Score','Prize']]
+    df= df.head(100)
 
     df[['University','Teams', 'Participants']] = df['Team'].str.extract(r'(.+)\((.+)\)\:(.+)')
 # Eliminar los caracteres ':' y ',' de la lista de participantes
@@ -40,6 +40,16 @@ def sracp(url,c):
     df["Anno"]=c
     df['Participants'] = df['Participants'].apply(convert_to_list)
     df.set_index('Teams', inplace=True)
+    df['Prize'] = df['Prize'].fillna(0)
+    for b in df['Prize']:
+            a=b.split("$")
+            try:
+                int(a[-1])
+                resultado = a[-1]
+            except ValueError:
+                resultado = "0"              
+            df['Prize'] = df['Prize'].replace(b, resultado)
+
     json2019 = df.T.to_json()
     return json2019
 json_data={}
@@ -52,5 +62,5 @@ for item in range(0,15):
     if data is not None:
         json_data[c] = data
 nombre_archivo = "datos.json"
-with open(nombre_archivo, "w") as archivo_json:
-    json.dump(json_data, archivo_json)
+with open(nombre_archivo, "w",encoding='utf-8') as archivo_json:
+    json.dump(json_data, archivo_json,ensure_ascii=False, indent=4)
