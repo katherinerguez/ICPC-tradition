dd8a47e3424a9a7eccf1b5bb516d5664cadb93ee
mariandc18 <ma907222@gmail.com>
2024-05-05 15:50:46 -0400

    scores and countries

--- web.py
+++ web.py
@@ -1,5 +1,7 @@
-import json 
-import pandas as pd 
+import json
+import pandas as pd
+import plotly.express as px
+import streamlit as st
 
 with open('datos.json','r') as a:
     archivos=json.load(a)
@@ -37,4 +39,21 @@ new_df=df
 new_df=new_df[new_df['University'].isin(top_50['University'])]
 new_df = new_df.reset_index(drop=True)
 
-        
+#Distribucion de universidades por paises 
+st.header("Distribución de las universidades por países", divider= "gray")
+selected_year = st.selectbox('Observa cuáles son los países con más universidades compitiendo en este evento en un anno en específico:', sorted(new_df['Anno'].unique()), key='selectbox_year1')
+filtered_data = new_df[new_df['Anno'] == selected_year]
+grouped_data = filtered_data.groupby('Country')['University'].count().reset_index()
+grouped_data = grouped_data.sort_values(by='University', ascending=False)
+
+fig = px.bar(grouped_data, x='Country', y='University', title=f'Cantidad de universidades por país en el año {selected_year}')
+st.plotly_chart(fig)
+
+#Promedio de scores por anno
+st.header("Scores en la competición", divider="gray")
+new_df['Score'] = pd.to_numeric(new_df['Score'], errors='coerce')
+new_df['Score'] = new_df['Score'].fillna(0).astype(int)
+avg_scores_by_year = new_df.groupby('Anno')['Score'].mean().reset_index()
+
+fig = px.bar(avg_scores_by_year, x='Anno', y='Score', title='Promedio de scores por año')
+st.plotly_chart(fig)
